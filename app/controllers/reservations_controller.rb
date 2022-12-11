64b8f2794617e8b516fa4859a66cf4ad1e90e6e8
ccbc7 <flexibleelbow@gmail.com>
2022-12-11 15:55:04 +0900

    hidden_fieldに関する質問。

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,7 @@
 class ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all
+    # binding.pry
   end
 
   def new
@@ -7,8 +9,9 @@ class ReservationsController < ApplicationController
   end
 
   def confirm
+
     # @reservation = Reservation.find(params[:id])
-    @reservation = Reservation.new(@reservation_params)
+    @reservation = Reservation.new(reservation_params)
     if @reservation.invalid? #入力項目に空のものがあれば入力画面に遷移
       render :new
     end
@@ -16,16 +19,17 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.new(@reservation_params)
+    @reservation = Reservation.new(reservation_params)
     # @event.user_id = current_user.id
     if params[:back] || !@reservation.save #戻るボタンを押したときまたは、@eventが保存されなかったらnewアクションを実行
       render :new and return
-    redirect_to  reservations_path
+    else
+      redirect_to  reservations_path
     end
   end
 
   private
   def reservation_params
-    @reservation_params = params.require(:reservation).permit(:id, :start_day)
+    params.permit(:start_day, :room_name)
   end
 end
